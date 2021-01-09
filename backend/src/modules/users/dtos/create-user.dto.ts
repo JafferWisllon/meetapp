e0c9364dc8a412ac6f7843a8c66bb1e28780354b
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-09 15:44:41 -0300

    validando os dados de entrada da rota de criação de usuário

--- backend/src/modules/users/dtos/create-user.dto.ts
+++ backend/src/modules/users/dtos/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Nome é obrigatório'
+  })
   name: string;
+
+  @IsEmail({}, {
+    message: 'Digite uma email válido'
+  })
   email: string;
+
+  @MinLength(6, {
+    message: 'Digite uma senha de no mínimo 6 dígitos'
+  })
   password: string;
 }
