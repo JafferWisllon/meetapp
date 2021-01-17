085f7f8bd5785d8d15dd8cfe2d140009076eeb75
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-17 15:24:20 -0300

    add eslint, prettier e editorconfig

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <View>
       <Text>Hello World</Text>
     </View>
-  )
+  );
 };
 
 export default App;
