22fcb15f808643459664cb7771449708fcac478f
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-19 20:03:01 -0300

    ativando o cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
+  
   const options = new DocumentBuilder()
   .setTitle('Meetapp API')
   .setDescription('The meetapp API description')
@@ -16,7 +17,7 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
+  
   await app.listen(3333);
 }
 bootstrap();
