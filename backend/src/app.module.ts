733faf65c063a5aee151ee89960b8977214f3e4f
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-19 20:32:26 -0300

    iniciando módulo de meetup no backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './modules/auth/auth.module';
+import { MeetupsModule } from './modules/meetups/meetups.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UsersModule,
-    AuthModule
+    AuthModule,
+    MeetupsModule
   ]
 })
 export class AppModule {}
---
+++ backend/src/modules/meetups/dtos/create-meetup.dto.ts
@@ -0,0 +1,8 @@
+export class CreateMeetupDto {
+  title: string;
+  description: string
+  locale: string;
+  date: Date; 
+  banner: string;
+  user_id: string;
+}
