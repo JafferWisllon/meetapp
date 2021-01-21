3e0d31e11d30ff46c77705b40278bcf666f16317
Jaffer Wisllon <contatojaffer@gmail.com>
2021-01-21 20:56:07 -0300

    não permitindo cadastrar meetups em datas que ja passaram

--- backend/src/modules/meetups/meetups.service.ts
+++ backend/src/modules/meetups/meetups.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/User.entity';
 import { CreateMeetupDto } from './dtos/create-meetup.dto';
 import { Meetup } from './entities/Meetup.entity';
+
+import { isBefore, parseISO } from 'date-fns'
 
 @Injectable()
 export class MeetupsService {
@@ -13,6 +15,12 @@ export class MeetupsService {
   ){}
   async create(user: any, {title, description, date, banner, locale}: CreateMeetupDto): Promise<Meetup> {
     const { userId } = user;
+
+    const dateHasPassed = isBefore(new Date(date), new Date());
+
+    if (dateHasPassed) {
+      throw new BadRequestException('Não é possivel criar meetups, essa data ja passou!')
+    }
 
     const meetup = this.meetupRepository.create({
       title, 
